7e9ec38b69e87f0a98276b464828a46849ef7dbe
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-06-11 01:43:53 +0530

    feat: basic setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {Text, View} from "react-native";
+import {Slot} from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const RootLayout = () => {
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    return (
+      <>
+       <Text>header</Text>
+         <Slot/>
+       <Text>Footer</Text>
+      </>
+    )
 }
+
+export default RootLayout
